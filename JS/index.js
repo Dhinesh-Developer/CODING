f4875f245cc7d6018439ccea3312ff24c18563d0
DHINESHKUMAR M <dhineshdeveloper07@gmail.com>
2025-02-07 18:29:34 +0530

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,104 @@
+console.log(`Hello`)
+console.log(`Dhinesh`)
+
+// window.alert(`This is an alert`)
+// window.alert(`I like pizza`)
+
+//This is an comment
+/*
+    This is an mulite line comment
+*/
+
+// document.getElementById("myH1").textContent="Hello";
+// document.getElementById("myP").textContent="dhinesh";
+
+
+// video ->2
+
+// variables  let x  =10;
+
+let x;
+x = 100;
+console.log(x);
+
+let y = 200;
+console.log(y);
+
+let age = 19;
+console.log(age);
+
+let price = 10.99;
+console.log(price);
+
+console.log(`You are ${age} years old`);
+console.log(`price is ${price}`);
+
+console.log(typeof age);
+
+let firstName = "Dhinesh";
+let lastName = "Kumar";
+console.log(`Myname is ${firstName} and ${lastName}`);
+
+var z = 300;
+console.log(z);
+
+const email = "Dk@Gmail.com";
+console.log(email);
+
+// age1 = true; // boolean
+// console.log(age1);
+
+let forSale = true;
+console.log(forSale);
+
+let online = false;
+console.log(`Dk is online: ${online}`);
+
+let fullName = "Dhinesh Kumar";
+let age1 = 19;
+let student = true;
+
+document.getElementById("p1").textContent= `You name is ${fullName}`;
+document.getElementById("p2").textContent= `Your age is ${age1}`;
+document.getElementById("p3").textContent= `You are a student : ${student}`;
+
+
+// arithmetic operators
+
+let students = 30;
+// students = students + 1;
+// students = students -1;
+// students = students * 1;
+// students  = students /1;
+// students = students ** 2;
+// students  = students %2;
+
+let extraStudents = 10;
+console.log(students + extraStudents);
+
+document.getElementById("student").textContent=`Student count = ${students}`
+
+let result = 1+2*3+4**2;
+let res = 6/128;
+console.log(res);
+console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
